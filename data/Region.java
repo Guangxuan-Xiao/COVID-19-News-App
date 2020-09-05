6d4644062418ddfe046e7171e2cd4c370c047a8d
Guangxuan-Xiao <xgx18@mails.tsinghua.edu.cn>
2020-09-05 16:19:35 +0800

    region json parser

---
+++ data/Region.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class Region {
+    public String name;
+    public String begin;
+    public List<List<Integer>> data;
+
+    Region(final String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Region{" +
+                "\nname='" + name + "\'" +
+                ", \nbegin='" + begin + "\'" +
+                ", \ndata=" + data +
+                "\n}";
+    }
+    //[COUNTRY|PROVINCE|COUNTY]:
+    //    {
+    //        "begin":"YYYY-mm-dd",
+    //            "data": [
+    //                  [CONFIRMED, SUSPECTED, CURED, DEAD, SEVERE, RISK, inc24]
+    //     ]}
+}
