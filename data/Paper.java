2bd6d1bf38e760523023f224c338e4ae7e3f894f
Guangxuan-Xiao <xgx18@mails.tsinghua.edu.cn>
2020-09-05 20:38:40 +0800

    news paper region Data Structure

---
+++ data/Paper.java
@@ -0,0 +1,41 @@
+import java.net.URL;
+import java.util.List;
+
+public class Paper {
+    class Author {
+        String name;
+        String id;
+
+        @Override
+        public String toString() {
+            return "Author{" +
+                    "\nname='" + name + '\'' +
+                    ", \nid='" + id + '\'' +
+                    "\n}";
+        }
+    }
+    public String title;
+    public String content;
+    public String seg_text;
+    public String date;
+    public String time;
+    public List<Author> authors;
+    public String doi;
+    public URL pdf;
+    public List<URL> urls;
+
+    @Override
+    public String toString() {
+        return "Paper{" +
+                "\ntitle='" + title + '\'' +
+                ", \ncontent='" + content + '\'' +
+                ", \nseg_text='" + seg_text + '\'' +
+                ", \ndate='" + date + '\'' +
+                ", \ntime='" + time + '\'' +
+                ", \nauthors=" + authors +
+                ", \ndoi='" + doi + '\'' +
+                ", \npdf=" + pdf +
+                ", \nurls=" + urls +
+                "\n}";
+    }
+}
