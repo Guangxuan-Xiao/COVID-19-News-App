6d53da2c3aa986434f38bb7e0661aff598c415f1
Guangxuan-Xiao <xgx18@mails.tsinghua.edu.cn>
2020-09-06 15:30:52 +0800

    data base

--- app/src/main/java/com/example/covid_news/data/Parser.java
+++ app/src/main/java/com/example/covid_news/data/Parser.java
@@ -1,11 +1,49 @@
 package com.example.covid_news.data;
 
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Parser {
-//    public List<News> loadNews(int page) {
-//
-//    }
-//
+    private Gson gson;
+    private JsonParser jsonParser;
+
+    public List<News> parseNewsList(String json) {
+        JsonObject query = jsonParser.parse(json).getAsJsonObject();
+        Type type = new TypeToken<List<News>>() {
+        }.getType();
+        List<News> newsList = gson.fromJson(query.getAsJsonArray("data"), type);
+        return newsList;
+    }
+
+    public List<Paper> parsePaperList(String json) {
+        JsonObject query = jsonParser.parse(json).getAsJsonObject();
+        Type type = new TypeToken<List<Paper>>() {
+        }.getType();
+        List<Paper> paperList = gson.fromJson(query.getAsJsonArray("data"), type);
+        return paperList;
+    }
+
+    public List<Region> parseEpidemic(String json) {
+        JsonObject regions = jsonParser.parse(json).getAsJsonObject();
+        List<Region> regionList = new ArrayList<>();
+        for (Map.Entry<String, JsonElement> en : regions.entrySet()) {
+            String name = en.getKey();
+            JsonObject regionJsonObject = regions.get(name).getAsJsonObject();
+            Region region = new Region(name);
+            region.begin = regionJsonObject.get("begin").getAsString();
+            JsonArray dataJsonArray = regionJsonObject.get("data").getAsJsonArray();
+            Type dataType = new TypeToken<List<List<Integer>>>() {
+            }.getType();
+            region.data = gson.fromJson(dataJsonArray, dataType);
+            regionList.add(region);
+        }
+        return regionList;
+    }
+
 
 }
